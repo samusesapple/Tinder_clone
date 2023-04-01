70b83c87e119e17f45d239c1fd452cee38280317
Sam Sung <126672733+samusesapple@users.noreply.github.com>
2023-04-01 18:14:43 +0900

    main UI 생성완료
    main UI 생성완료

--- TinderClone/TinderClone/View/CardView.swift
+++ TinderClone/TinderClone/View/CardView.swift
@@ -10,11 +10,29 @@ import UIKit
 class CardView: UIView {
     
     // MARK: - Properties
+    private let gradientLayer = CAGradientLayer()
+    
     private let imageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
-        iv.image = #imageLiteral(resourceName: "jane2")
+        iv.image = #imageLiteral(resourceName: "jane3")
         return iv
+    }()
+    
+    private let infoLabel: UILabel = {
+       let label = UILabel()
+        label.numberOfLines = 2
+        
+        let attributedText = NSMutableAttributedString(string: "Jane Doe", attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy), .foregroundColor: UIColor.white])
+        attributedText.append(NSMutableAttributedString(string: "  21", attributes: [.font: UIFont.systemFont(ofSize: 24), .foregroundColor: UIColor.white]))
+        label.attributedText = attributedText
+        return label
+    }()
+    
+    private lazy var infoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "info_icon").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
     }()
     
     // MARK: - Lifecycle
@@ -27,12 +45,33 @@ class CardView: UIView {
         
         addSubview(imageView)
         imageView.fillSuperview()
+        setGradient()
+        
+        addSubview(infoLabel)
+        infoLabel.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingLeft: 16, paddingBottom: 16, paddingRight: 16)
+        
+        addSubview(infoButton)
+        infoButton.setDimensions(height: 40, width: 40)
+        infoButton.centerY(inView: infoLabel)
+        infoButton.anchor(right: rightAnchor, paddingRight: 16)
+    }
+    
+    // 프레임의 생성이 끝난 후의 시점, 그라데이션을 프레임 기준으로 잡아야함
+    override func layoutSubviews() {
+        gradientLayer.frame = self.frame
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+
     
+    func setGradient() {
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1.1]
+        layer.addSublayer(gradientLayer)
+
+    }
     
 }
